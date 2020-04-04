c267f8c76a9bfab1ffe1c091bab4d261ea6c6b62
Payam Dibaeinia <p.dibaeinia@gmail.com>
2020-04-04 18:50:51 -0500

    First version

---
+++ utils.py
@@ -0,0 +1,25 @@
+"""
+@author: Payam Dibaeinia
+"""
+
+import os
+
+
+def make_dir(path, writable = False):
+    """
+    writable: determines if the given path points to a readable/writable file, if
+    so, only the intermidiate folders are made
+    """
+    if os.path.exists(path):
+        print("directory " + path + " already exists", flush = True)
+
+    if writable:
+        path_list = path.split('/')[:-1]
+
+        if path[0] == '/':
+            path = '/' + '/'.join(path_list)
+        else:
+            path = '/'.join(path_list)
+
+    if not os.path.exists(path):
+        os.mkdir(path)
